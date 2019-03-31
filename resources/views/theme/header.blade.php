0368d0ac684bee93dc1d2ddc672b964bb0168913
Iva Conte <ivconte@gmail.com>
2019-03-31 19:05:06 -0300

    logout

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -33,16 +33,6 @@
 
         <ul class="dropdown-menu dropdown-user">
 
-            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-
-            </li>
-
-            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-
-            </li>
-
-            <li class="divider"></li>
-
             <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out fa-fw"></i> Sair</a>
 
             </li>
