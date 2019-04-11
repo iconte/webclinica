a976ca1eedb2648c056ffcabbc739e474516e3fd
Iva Conte <ivconte@gmail.com>
2019-04-11 11:02:08 -0300

    typeahead timeout

--- public/js/consulta.js
+++ public/js/consulta.js
@@ -1,46 +1,52 @@
 $(function () {
-    $.get('/api/medico',function(resultado){
-        var data= resultado.data;
-        if(data){
-            data.sort(function(a, b){
-                if(a.pessoa.nome < b.pessoa.nome) { return -1; }
-                if(a.pessoa.nome > b.pessoa.nome) { return 1; }
+    var timeout;
+    $.get('/api/medico', function (resultado) {
+        var data = resultado.data;
+        if (data) {
+            data.sort(function (a, b) {
+                if (a.pessoa.nome < b.pessoa.nome) {
+                    return -1;
+                }
+                if (a.pessoa.nome > b.pessoa.nome) {
+                    return 1;
+                }
                 return 0;
             });
             $("#lista_medicos").append('<option value="" selected>Selecione</option>');
 
-            for(var i = 0;i<data.length;i++){
-                $("#lista_medicos").append('<option value="'+data[i].id+'">'+ data[i].pessoa.nome+'</option>');
+            for (var i = 0; i < data.length; i++) {
+                $("#lista_medicos").append('<option value="' + data[i].id + '">' + data[i].pessoa.nome + '</option>');
 
             }
         }
     });
 
-        $('#med').typeahead({
-            hint: true,
-            highlight: true,
-            minLength: 4,
-            source: function (query, result) {
-                $.ajax({
-                    url: "/api/medicamento/nome/"+query,
-                    type: "GET",
-                    success: function (resultado) {
-                        result($.map(resultado.data, function (item) {
-                            //var itm = ''
-                            //+ "<div class='typeahead_wrapper'>"
-                            //+ "<div class='typeahead_labels'>"
-                            //+ "<div class='typeahead_primary'>" + item.nome_fabrica + "</div>"
-                            //+ "<div class='typeahead_secondary'><small>" + item.nome_generico +"</small></div>"
-                            //+ "<div class='typeahead_secondary'><small>" + item.apresentacao+ "</small></div>"
-                            //+ "</div>"
-                            //+ "</div>";
-                            //return itm;
-                            return item.nome_fabrica + ' '+item.apresentacao;
-                        }));
-                    },
-                });
-            },
-        },800);
+    $('#med').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 4,
+        source: function (query, result) {
+            $.ajax({
+                url: "/api/medicamento/nome/" + query,
+                type: "GET",
+                success: function (resultado) {
+                    result($.map(resultado.data, function (item) {
+                        //var itm = ''
+                        //+ "<div class='typeahead_wrapper'>"
+                        //+ "<div class='typeahead_labels'>"
+                        //+ "<div class='typeahead_primary'>" + item.nome_fabrica + "</div>"
+                        //+ "<div class='typeahead_secondary'><small>" + item.nome_generico +"</small></div>"
+                        //+ "<div class='typeahead_secondary'><small>" + item.apresentacao+ "</small></div>"
+                        //+ "</div>"
+                        //+ "</div>";
+                        //return itm;
+                        return item.nome_fabrica + ' ' + item.apresentacao;
+                    }));
+                }
+            });
+        },
+
+    },400);
 
     $('#ex').typeahead({
         hint: true,
@@ -48,17 +54,17 @@ $(function () {
         minLength: 4,
         source: function (query, result) {
             $.ajax({
-                url: "/api/exame/nome-codsus/"+query,
+                url: "/api/exame/nome-codsus/" + query,
                 type: "GET",
                 success: function (resultado) {
 
                     result($.map(resultado.data, function (item) {
-                        return item.cod_sus + ' - '+ item.descricao;
+                        return item.cod_sus + ' - ' + item.descricao;
                     }));
                 }
             });
         }
-    },800);
+    }, 400);
 
 
 });
@@ -86,8 +92,8 @@ $("#btnSalvarConsulta").click(function (event) {
         },
         success: function (data) {
 
-            if(data){
-                if(data.errors){
+            if (data) {
+                if (data.errors) {
                     msgValidacao(data.errors);
                     return;
                 }
